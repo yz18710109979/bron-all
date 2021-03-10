3e98699662034ed23525ef25dee6856f1edd7ea4
yz18710109979 <79241484@qq.com>
2021-03-10 21:07:34 +0800

    xiufu

--- utils/src/main/java/com/jy/service/HDFSService.java
+++ utils/src/main/java/com/jy/service/HDFSService.java
@@ -6,7 +6,6 @@ import org.apache.hadoop.fs.*;
 import org.apache.hadoop.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
