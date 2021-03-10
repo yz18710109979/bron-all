3c012e29a379338b2553f67d99cc83e3c9978e09
yz18710109979 <79241484@qq.com>
2021-03-10 21:01:07 +0800

    mapreduce

---
+++ utils/src/main/java/com/jy/mapred/SignNumberMax.java
@@ -0,0 +1,16 @@
+package com.jy.mapred;
+
+import java.util.Map;
+
+// 多个文件中的数字最大值
+public class SignNumberMax {
+    public static void main(String[] args) {
+
+    }
+    public static Map<String, Integer> readFile(String path) {
+        return null;
+    }
+    public static Map<String,Integer> mergeFile(Map<String, Integer> ...maps) {
+        return null;
+    }
+}
---
+++ utils/src/main/java/com/jy/mapred/SignWordCount.java
@@ -0,0 +1,18 @@
+package com.jy.mapred;
+
+import java.util.Map;
+
+// 找出文件中单次的出现次数
+public class SignWordCount {
+    public static void main(String[] args) {
+
+    }
+    public static Map<String, Integer> readFile(String path) {
+
+        return null;
+    }
+
+    public static Map<String, Integer> mergeAllFile(Map<String, Integer> ...maps) {
+        return null;
+    }
+}
