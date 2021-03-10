167565ef83fefc4ff522878647559fd7e60f08b3
yz18710109979 <79241484@qq.com>
2021-03-10 21:09:54 +0800

    map01

--- utils/src/main/java/com/jy/mapred/SignNumberMax.java
+++ utils/src/main/java/com/jy/mapred/SignNumberMax.java
@@ -5,7 +5,7 @@ import java.util.Map;
 // 多个文件中的数字最大值
 public class SignNumberMax {
     public static void main(String[] args) {
-
+        Map<String, Integer> map01 = readFile("");
     }
     public static Map<String, Integer> readFile(String path) {
         return null;
